a180923486100d1da6448a9f7d16c72ff617f952
Komal-J04 <komal.jain2k04@gmail.com>
2023-02-11 14:16:29 +0530

    Enjoy the game!

--- script.js
+++ script.js
@@ -12,15 +12,29 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
-//Initial conditions
-score0El.textContent = 0; //JS automatically converts them to strings to display them on the page
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//Initial conditions
+function init() {
+  score0El.textContent = 0; //JS automatically converts them to strings to display them on the page
+  score1El.textContent = 0;
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+}
 
 function switchPlayer() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -29,6 +43,8 @@ function switchPlayer() {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 }
+
+init();
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -61,7 +77,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2.Check if score>=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       diceEl.classList.add('hidden');
       playing = false;
@@ -77,3 +93,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
